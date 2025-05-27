da3f08c45b701562128c636daf9f16c1244b9d91
Erik Alonso <ealoh@Eriks-MacBook-Pro.local>
2025-05-26 22:19:49 -0600

    Changed secret key, secured in .env, added .env.template, _init_ changes :lock:

--- market/__init__.py
+++ market/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv() #Init the dotEnv ext to secure the key in public
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = '213cff89e45ae95fc68bf9c8'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback-dev-key')
 db = SQLAlchemy(app) # basically we specify the DB manager SQLAlchemy will work on this particular app scope
 bcrypt = Bcrypt(app) # basically we specify the encryption will work on this particular app scope
 
--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 from market import app
+from flask import Flask
+import os
+
+@app.route('/')
+def home():
+    return "Hello from Flask!"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get('PORT', 5050))
+    app.run(host='0.0.0.0', port=port)
+
